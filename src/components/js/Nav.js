634242b25d45bf8a0eda9ec60bdb2c8767eb64bb
Matthew <mjford95@gmail.com>
2020-05-30 13:26:28 +1200

    added smoe mobile aspects final product

--- src/components/js/Nav.js
+++ src/components/js/Nav.js
@@ -7,7 +7,7 @@ export default function Nav() {
     return (
         <div className="nav-container">
             <nav>
-            <NavLink to="/works"  className = "logo">Logo</NavLink>
+            <NavLink to="/works"  className = "logo">M.F</NavLink>
             <NavLink to="/works"  className = "nav-link" activeStyle={{textDecoration: "line-through"}}>Works</NavLink>
             <NavLink to="/about" className = "nav-link" activeStyle={{textDecoration: "line-through"}} >About</NavLink>
             </nav>
